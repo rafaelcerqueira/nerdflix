9d05efbae1b49b3c5ff907c4b36731110a594955
Rafael Cerqueira <rafaelcerqueira92@gmail.com>
2020-08-10 11:21:50 -0300

    starting Nerdflix aplication

---
+++ src/components/ButtonNerdflix/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ButtonNerdflix = styled.button `
+    padding: 18px 40px;
+    background-color: var(--color-black-dark);
+
+`;
+
+export default ButtonNerdflix;
---
+++ src/components/LogoNerdflix/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import logo from '../../assets/img/Logo.png';
+
+const Logo = styled.img.attrs({ src: logo, alt: 'Logo da Nerdflix' })`
+    height: 40px;
+`;
+
+export default Logo;
