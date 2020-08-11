33e433a4eb9a63ebb8a8f8d91a736202426ddc0f
Rafael de Cerqueira Freitas <rafaelcerqueira92@gmail.com>
2020-08-11 11:02:50 -0300

    Fixing Banner for desktop version

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LogoNerdflix from './components/LogoNerdflix';
 import ButtonNerdflix from './components/ButtonNerdflix';
 import FooterNerdflix from './components/FooterNerdflix';
 import LogoImersao from './components/LogoImersao';
-import BannerNerdflix from './components/BannerNerdflix';
+import { BannerNerdflix, Text } from './components/BannerNerdflix';
 import TagNerdflix from './components/TagNerdflix';
 import TitleNerdflix from './components/TitleNerdflix';
 import DescriptionNerdflix from './components/DescriptionNerdflix';
@@ -24,11 +24,13 @@ function App() {
       </ HeaderNerdflix>
 
       <BannerNerdflix>
-        <TagNerdflix>Tech</TagNerdflix>
-        <TitleNerdflix>Conheça o canal da Alura!</TitleNerdflix>
-        <DescriptionNerdflix>
-          Acompanhe conteúdos de Tecnologia, Design e Negócios digitais tanto em aulas curtas e práticas, quanto em webséries, como o Hipsters Ponto Tube. Conheça a Alura: <a href="https://www.alura.com.br/">alura.com.br</a>
-        </DescriptionNerdflix>
+        <Text>
+          <TagNerdflix>Tech</TagNerdflix>
+          <TitleNerdflix>Conheça o canal da Alura!</TitleNerdflix>
+          <DescriptionNerdflix>
+            Acompanhe conteúdos de Tecnologia, Design e Negócios digitais tanto em aulas curtas e práticas, quanto em webséries, como o Hipsters Ponto Tube. Conheça a Alura: <a href="https://www.alura.com.br/">alura.com.br</a>
+          </DescriptionNerdflix>
+        </Text>
 
         <ThumbNerdflix src={ConhecaAlura} alt="Thumb do Conheça a Alura"/>
       </BannerNerdflix>
--- src/components/BannerNerdflix/index.js
+++ src/components/BannerNerdflix/index.js
@@ -1,8 +1,28 @@
 import styled from 'styled-components';
 import container from '../../tools/container';
+import TagNerdflix from '../TagNerdflix';
+import TitleNerdflix from '../TitleNerdflix';
+import ThumbNerdflix from '../ThumbNerdflix';
 
-const BannerNerdflix = styled.section `
-    ${container};
+export const Text = styled.div `
+    padding-top: 10rem;
+    max-width: 45%;
+
+    ${TagNerdflix} {
+        margin-bottom: 40rem;
+    }
+
+    ${TitleNerdflix} {
+        margin-bottom: 20px;
+    }
 `;
 
-export default BannerNerdflix;
+export const BannerNerdflix = styled.section `
+    ${container};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-height: 70vh;
+    height: 100vh;
+`;
+
