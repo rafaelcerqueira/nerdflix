e2c66988400c91484a8f52e2e1c9e900ce6cf9a7
Rafael de Cerqueira Freitas <rafaelcerqueira92@gmail.com>
2020-08-16 13:11:54 -0300

    Thumb video animation added in home

--- src/components/ThumbNerdflix/styles.js
+++ src/components/ThumbNerdflix/styles.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 
 
 export const Avatar = styled.img `
-    --space: 10rem;
     position: absolute;
     top: var(--space);
     left: var(--space);
@@ -22,18 +21,24 @@ export const Thumb = styled.img `
 `;
 
 export const WrapperThumb = styled.figure `
+    --space: 10rem;
     position: relative;
     border-radius: 4px;
     border: 4rem solid var(--color-frontend);
     width: 640px;
     overflow: hidden;
     cursor: pointer;
+    transition: transform 100ms linear;
 
     &:hover {
+        --move: calc(var(--space) * -1);
+        transform: translate(var(--move), var(--move));
+        
+
         & > ${Avatar} {
             transform: translateX(0);
             opacity: 1;
-            transition: transform 125ms linear, opacity 290ms linear;
+            transition: transform 125ms  150ms linear, opacity 290ms 100ms linear;
         }
     }
 `;
