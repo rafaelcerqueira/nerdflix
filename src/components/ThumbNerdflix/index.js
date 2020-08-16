b438db70c0fcc7aeaa769ac1b0b9815e6e31db94
Rafael de Cerqueira Freitas <rafaelcerqueira92@gmail.com>
2020-08-16 16:09:39 -0300

    Thumb out of the box effect added.

--- src/components/ThumbNerdflix/index.js
+++ src/components/ThumbNerdflix/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { WrapperThumb, Thumb, Avatar } from './styles';
+import { Background, WrapperThumb, Thumb, Avatar } from './styles';
 
 function ThumbNerdflix({ src, alt, avatar, channelName }) {
     
     return (
-        <WrapperThumb>
-            <Thumb src={src}  alt={alt} />
-            <Avatar src={avatar} alt={channelName} />
-        </WrapperThumb>
+        <Background>
+            <WrapperThumb>
+                <Thumb src={src}  alt={alt} />
+                <Avatar src={avatar} alt={channelName} />
+            </WrapperThumb>
+        </Background>
     );
 
 }
--- src/components/ThumbNerdflix/styles.js
+++ src/components/ThumbNerdflix/styles.js
@@ -21,24 +21,70 @@ export const Thumb = styled.img `
 `;
 
 export const WrapperThumb = styled.figure `
-    --space: 10rem;
+
+    /* opacity: 0; */
     position: relative;
-    border-radius: 4px;
-    border: 4rem solid var(--color-frontend);
+    /*    border-radius: 4px; */
+    border: var(--border) solid var(--color-frontend);
     width: 640px;
     overflow: hidden;
     cursor: pointer;
     transition: transform 100ms linear;
+`;
+
+export const Background = styled.div`
+    --space: 10rem;
+    --border: 4rem;
+    --move-space: calc(var(--space) * -1);
+    
+    position: relative;
+    background-color: var(--color-frontend);
+
+    &::before,
+    &::after {
+        content: '';
+        position: absolute;
+        
+        width: calc(var(--space) * 1.4);
+        height: calc(var(--space) * 1.4);
+        background-color: var(--color-frontend);
+        transition: transform 100ms linear;
+        
+    }
+
+    &::before {
+        right: 0;
+        top: 0;
+        transform-origin: right top;
+        transform: rotate(45deg) scale(0);
+
+    }
+
+    &::after{
+        left:0;
+        bottom: 0;
+        transform-origin: left bottom;
+        transform: rotate(-45deg) scale(0);
+        z-index: -1;
+    }
 
     &:hover {
-        --move: calc(var(--space) * -1);
-        transform: translate(var(--move), var(--move));
-        
+        &::before {
+            transform: rotate(45deg) scale(1);
+        }
+        &::after {
+            transform: rotate(-45deg) scale(1);
+        }
 
-        & > ${Avatar} {
-            transform: translateX(0);
-            opacity: 1;
-            transition: transform 125ms  150ms linear, opacity 290ms 100ms linear;
+
+        & > ${WrapperThumb} {
+            transform: translate(var(--move-space), var(--move-space));
+            
+            & > ${Avatar} {
+                transform: translateX(0);
+                opacity: 1;
+                transition: transform 125ms  150ms linear, opacity 290ms 100ms linear;
+            }   
         }
     }
 `;
